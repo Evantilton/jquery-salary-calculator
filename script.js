b1ec206088e968c257253147e7978bb7dd18e837
Evan Tilton <evantilton@gmail.com>
2019-10-05 02:53:09 -0500

    working on the delete button removing from array, found the name and the index but can't remove from array yet without breaking

--- script.js
+++ script.js
@@ -17,10 +17,15 @@ function inputInfo() {
     const jobTitle = $('#jobTitleInput').val();
     const annualSalaryValue = $('#annualSalaryInput').val();
 
+
+
+
+
     console.log(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue);
+    storingSalary(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue);
     $('tbody').append(`
     <tr>
-        <td>${nameValue}</td>
+        <td class='name'>${nameValue}</td>
         <td>${lastNameValue}</td>
         <td>${idValue}</td>
         <td>${jobTitle}</td>
@@ -29,17 +34,28 @@ function inputInfo() {
         <button class="deleteButton"> delete </button>
         </td>
     </tr>`);
-    storingSalary(Number(annualSalaryValue));
+
     $('#totalMonthly').text(calculateTotal);
 }
 
 function deleteInfo() {
+    //test
+    let val = $(this).closest('tr').find(".name").text();
+    console.log("val", val);
+    // let index = storedSalary.find { return storedSalary.name == val});
+    // console.log("index", index);
+    deleteFromArray(val, storedSalary);
+    console.log("stored salary", storedSalary);
+
+
+    //test
     console.log("in deleteInfo");
     $(this).parent().parent().remove();
+    $('#totalMonthly').text(calculateTotal);
 }
 
-function storingSalary(annualSalaryValue) {
-    storedSalary.push(annualSalaryValue);
+function storingSalary(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue) {
+    storedSalary.push({ name: nameValue, lastName: lastNameValue, id: idValue, title: jobTitle, salary: annualSalaryValue });
     console.log(storedSalary);
 }
 
@@ -47,8 +63,22 @@ function calculateTotal() {
     let totalEverything = 0
     console.log("in calculate total")
     for (let i = 0; i < storedSalary.length; i++) {
-        console.log(storedSalary[i]);
-        totalEverything = Number(totalEverything)+(storedSalary[i]);
+        console.log(Number(storedSalary[i].salary));
+        totalEverything = Number(totalEverything) + (Number(storedSalary[i].salary));
     }
     return totalEverything;
+}
+
+function deleteFromArray(val, otherVal) {
+    let oldData = [];
+    console.log("deleting from Array");
+    for (let i = 0; i < storedSalary.length; i++) {
+        if (val == otherVal[i].name) {
+            console.log("deletingStoredSalary", otherVal[i].salary);
+            otherVal.splice(otherVal[i]);
+            console.log(otherVal);
+            console.log(storedSalary);
+
+        }
+    }
 }
