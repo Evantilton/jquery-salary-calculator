3d081ad342bd6c7c4e83bb8d2446cafd10daea54
Evan Tilton <evantilton@gmail.com>
2019-10-05 00:02:44 -0500

    finished the total monthly and got it to display on the dom

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 console.log("in script.js")
-
+let storedSalary = []
 $(document).ready(readyNow);
-    
+
 
 function readyNow() {
     console.log("in readyNow");
     $('#submitButton').on('click', inputInfo);
-    $('tbody').on('click','.deleteButton', deleteInfo);
+    $('tbody').on('click', '.deleteButton', deleteInfo);
 }
 
-function inputInfo(){
+function inputInfo() {
     console.log("in inputField");
     const nameValue = $('#firstNameInput').val();
     const lastNameValue = $('#lastNameInput').val();
     const idValue = $('#employeeNumberInput').val();
     const jobTitle = $('#jobTitleInput').val();
     const annualSalaryValue = $('#annualSalaryInput').val();
-    console.log(nameValue,lastNameValue,idValue,jobTitle, annualSalaryValue);
+
+    console.log(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue);
     $('tbody').append(`
     <tr>
         <td>${nameValue}</td>
@@ -28,9 +29,26 @@ function inputInfo(){
         <button class="deleteButton"> delete </button>
         </td>
     </tr>`);
+    storingSalary(Number(annualSalaryValue));
+    $('#totalMonthly').text(calculateTotal);
 }
 
-function deleteInfo(){
+function deleteInfo() {
     console.log("in deleteInfo");
     $(this).parent().parent().remove();
+}
+
+function storingSalary(annualSalaryValue) {
+    storedSalary.push(annualSalaryValue);
+    console.log(storedSalary);
+}
+
+function calculateTotal() {
+    let totalEverything = 0
+    console.log("in calculate total")
+    for (let i = 0; i < storedSalary.length; i++) {
+        console.log(storedSalary[i]);
+        totalEverything = Number(totalEverything)+(storedSalary[i]);
+    }
+    return totalEverything;
 }
