d3f68df332d3f998b00dfbfc63d0487b9452b805
Evan Tilton <evantilton@gmail.com>
2019-10-07 10:54:14 -0500

    more formatting fixes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Description
 Duration: Weekend
 
-    This application is for an employer to keep track of employees and monthly budget.  It allows the user to input the employees first and last name, ID number, job title, and annual salary.  It assumes $20,000 is the monthly alloted budget and informs the user if the they have gone over that budget.  It also allows the user to delete the inputed employee (off of Employee ID) and properly tracks that current budget after the employee is deleted.
+    This application is for a user to keep track of employees and monthly budget.  It allows the user to input the employees first and last name, ID number, job title, and annual salary.  It assumes $20,000 is the monthly alloted budget and informs the user if the they have gone over that budget.  It also allows the user to delete the inputed employee (off of Employee ID) and properly tracks that current budget after the employee is deleted.
 
     To see the fully functional site, please visit: https://github.com/Evantilton/jquery-salary-calculator
 
@@ -16,7 +16,12 @@ Duration: Weekend
 ## Installation
     fork and clone the repo
     open index.html
+## Usage
 
+1. Follow the steps in installation.
+2. Fill in each input box for the employee.
+3. Click submit.
+4. If needed or overbudget, used the delete button to remove an employee.
 
 ## Built With
     html
--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 console.log("in script.js")
 let storedSalary = [];
-$(document).ready(readyNow);
+//this array needs to be global
 
+$(document).ready(readyNow);
 
 function readyNow() {
     console.log("in readyNow");
     $('#submitButton').on('click', inputInfo);
+    //this inputs the employee info into the table and 
+    //adds it into an array as an object
+    //and then calculates a total based off the 
+    //total salaries
     $('tbody').on('click', '.deleteButton', deleteInfo);
+    //this deletes the info on delete button press 
+    //and fixes the Monthly Total
     $('#annualSalaryInput').keyup(function () { 
         this.value = this.value.replace(/[^0-9\.]/g,'');
     });//I wanted a way to keep out bad inputs
@@ -24,10 +31,10 @@ function inputInfo() {
     $('#employeeNumberInput').val('');
     $('#jobTitleInput').val('');
     $('#annualSalaryInput').val('');
-
+    //I could probably target all these instead of making them copy but this works
     if ((nameValue==='')||(lastNameValue==='')||(idValue==='')||(jobTitle==='')||(annualSalaryValue==='')){
         return alert("Please fill in all fields");
-        //This is to make sure they fill in all fields.
+        //This is to make sure they fill in all fields. 
     } if  ((annualSalaryValue))
     console.log(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue);
     storingSalary(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue);
@@ -55,14 +62,18 @@ function deleteInfo() {
     console.log("in deleteInfo");
     $(this).parent().parent().remove();
     $('#totalMonthly').text(calculateTotal);
+    //this calculateTotal is to reset the new Monthly 
+    //Total after the employee is deleted.
 } // end deleteInfo
 
 function storingSalary(nameValue, lastNameValue, idValue, jobTitle, annualSalaryValue) {
+    //this creates an object for the new employee.
     storedSalary.push({ name: nameValue, lastName: lastNameValue, id: idValue, title: jobTitle, salary: annualSalaryValue });
     console.log(storedSalary);
 }// end storingSalary
 
 function calculateTotal() {
+    //this function calculates the sum of the stored salaries.
     let totalEverything = 0
     $('h3').css('background-color','white');
     console.log("in calculate total")
@@ -78,11 +89,15 @@ function calculateTotal() {
 } // end calculateTotal
 
 function deleteFromArray(val, storedSalary) {
+    //then sets the deleted employee's salary to 0
+    //so that the sum can be calculated properly
+    //to account for the now missing employee.
     console.log("deleting from Array");
     for (let i = 0; i < storedSalary.length; i++) {
         if (val == storedSalary[i].id) {
             console.log("deletingStoredSalary", storedSalary[i].salary);
             storedSalary[i].salary= 0;
+            //this sets the Salary to 0 for the deleted employee
             console.log("newStoredSalary", storedSalary[i]);
             console.log("full", storedSalary);
 
@@ -93,5 +108,5 @@ function deleteFromArray(val, storedSalary) {
 function makeCurrency(numberToMakeCurrency){
     let currency = ('$') + parseFloat(numberToMakeCurrency).toFixed(2);
     return currency
+    //this function is to make it into currency format.
 } //end makeCurrency
-
